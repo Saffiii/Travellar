1880b51624819c7687c6042fbb118042618d8d1a
Saffiii <yousaf.idrees@kurtosys.com>
2018-04-19 19:30:29 +0100

    Updated static content
    re-colleced all static content

--- Travellar/static/frontend/js/sabre.js
+++ Travellar/static/frontend/js/sabre.js
@@ -13,7 +13,7 @@ $.ajaxSetup({
 });
 
 $(function () {
-    var flights, index;
+    var flights, index, hotels, rentals;
 
     $.ajax({
         type: "GET",
@@ -21,9 +21,9 @@ $(function () {
         success: function (response) {
             //console.log(response)
             flights = response.data.destinations;
+            hotels = response.data.hotels;
+            rentals = response.data.rentals;
             index = response.data.topDestination.Destinations
-            console.log(flights)
-            console.log(index)
 
             if (window.location.pathname === '/flights') {
                 updateFlightsView(flights)
@@ -33,20 +33,13 @@ $(function () {
                 updateIndexView(index)
             }
 
+            if (window.location.pathname === '/hotel') {
+                updateHotelView(hotels)
+            }
 
-            // for (var key in response.data.Destinations) {
-            //     //console.log(response.data.Destinations[key].Destination)
-            //     var datarow = response.data.Destinations[key].Destination
-            //     if (datarow.CityName){
-            //         console.log(datarow.CityName)
-            //     }
-            //     else if (datarow.MetropolitanAreaName){
-            //         console.log(datarow.MetropolitanAreaName)
-            //     }
-            //     else{
-            //         console.log(datarow.RegionName)
-            //     }
-            // }
+            if (window.location.pathname === '/rental') {
+                updateRentalView(rentals)
+            }
         },
         error: function () {
             console.log('Error')
@@ -56,18 +49,117 @@ $(function () {
 
 
 function updateFlightsView(data){
-    $('.banners').append("<div class=\"grid_4\">" +
-        "                    <div class=\"banner\">" +
-        "                        <img src= 'static/frontend/img/page2_img1.jpg'  alt=\"\">" +
-        "                        <div class=\"label\">" +
-        "                            <div class=\"title\">"+data['0'].DestinationLocation+"</div>" +
-        "                            <div class=\"price\">from<span>$ 1.200</span></div>" +
-        "                            <a href=\"#\">LEARN MORE</a>" +
-        "                        </div>" +
-        "                    </div>" +
-        "                </div>");
+    console.log(data)
+    for (var key in data) {
+
+        if (data.hasOwnProperty(key)) {
+            if(data[key].DestinationLocation && data[key].LowestFare.Fare) {
+                var destination = data[key].DestinationLocation;
+                var price = data[key].LowestFare.Fare.toString() + " " + data[key].CurrencyCode;
+                // if (key < 8) {
+                $('.banners').append("<div class=\"grid_4\">" +
+                    "                    <div class=\"banner\">" +
+                    "                        <img src= 'static/frontend/img/page2_img1.jpg'  alt=\"\">" +
+                    "                        <div class=\"label\">" +
+                    "                            <div class=\"title\">" + destination + "</div>" +
+                    "                            <div class=\"price\">from<span>" + price + "</span></div>" +
+                    "                            <a href=\"#\">LEARN MORE</a>" +
+                    "                        </div>" +
+                    "                    </div>" +
+                    "                </div>");
+                // }
+            }
+        }
+    }
 }
 
-function updateIndexView(){
+function updateIndexView(data){
+    for (var key in data) {
+        let datarow = data[key].Destination;
+        let destination;
+        let country = datarow.CountryName;
 
+        if (datarow.CityName){
+            destination = datarow.CityName
+        }
+        else if (datarow.MetropolitanAreaName){
+            destination = datarow.MetropolitanAreaName
+        }
+        else{
+            destination = datarow.RegionName
+        }
+        $('.popular').append("<div class=\"grid_4\">" +
+            "                    <div class=\"banner\">" +
+            "                        <img src= 'static/frontend/img/page2_img1.jpg'  alt=\"\">" +
+            "                        <div class=\"label\">" +
+            "                            <div class=\"title\">" + destination + "</div>" +
+            "                            <div class=\"price\"><span>" + country + "</span></div>" +
+            "                            <a href=\"#\">LEARN MORE</a>" +
+            "                        </div>" +
+            "                    </div>" +
+            "                </div>");
+        // if(key < 8) {
+        //     $('.recommendedbyus').append("<div class=\"grid_4\">" +
+        //         "                    <div class=\"banner\">" +
+        //         "                        <img src= 'static/frontend/img/page2_img1.jpg'  alt=\"\">" +
+        //         "                        <div class=\"label\">" +
+        //         "                            <div class=\"title\">" + destination + "</div>" +
+        //         "                            <div class=\"price\"><span>" + country + "</span></div>" +
+        //         "                            <a href=\"#\">LEARN MORE</a>" +
+        //         "                        </div>" +
+        //         "                    </div>" +
+        //         "                </div>");
+        // }
+        // else{
+        //     $('.popular').append("<div class=\"grid_4\">" +
+        //         "                    <div class=\"banner\">" +
+        //         "                        <img src= 'static/frontend/img/page2_img1.jpg'  alt=\"\">" +
+        //         "                        <div class=\"label\">" +
+        //         "                            <div class=\"title\">" + destination + "</div>" +
+        //         "                            <div class=\"price\"><span>" + country + "</span></div>" +
+        //         "                            <a href=\"#\">LEARN MORE</a>" +
+        //         "                        </div>" +
+        //         "                    </div>" +
+        //         "                </div>");
+        // }
+    }
+}
+
+function updateHotelView(data) {
+    for (let key in data) {
+        let name = data[key].name;
+        let city = data[key].city;
+        let country = data[key].country;
+        $('.banners').append("<div class=\"grid_4\">" +
+            "                    <div class=\"banner\">" +
+            "                        <img src= 'static/frontend/img/page2_img1.jpg'  alt=\"\">" +
+            "                        <div class=\"label\">" +
+            "                            <div class=\"title\">" + name + "</div>" +
+            "                            <div class=\"price\"><span>" + city + "</span></div>" +
+            "                            <div class=\"price\"><span>" + country + "</span></div>" +
+            "                            <a href=\"#\">LEARN MORE</a>" +
+            "                        </div>" +
+            "                    </div>" +
+            "                </div>");
+    }
+}
+
+
+function updateRentalView(data) {
+    for (let key in data) {
+        let company = data[key].company;
+        let city = data[key].city;
+        let country = data[key].country;
+        $('.banners').append("<div class=\"grid_4\">" +
+            "                    <div class=\"banner\">" +
+            "                        <img src= 'static/frontend/img/page2_img1.jpg'  alt=\"\">" +
+            "                        <div class=\"label\">" +
+            "                            <div class=\"title\">" + company + "</div>" +
+            "                            <div class=\"price\"><span>" + city + "</span></div>" +
+            "                            <div class=\"price\"><span>" + country + "</span></div>" +
+            "                            <a href=\"#\">LEARN MORE</a>" +
+            "                        </div>" +
+            "                    </div>" +
+            "                </div>");
+    }
 }
